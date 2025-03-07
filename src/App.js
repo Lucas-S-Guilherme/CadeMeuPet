2573c8ff30f2c253883122932b81175b8a30477a
Lucas Guilherme <lucassguilherme159@gmail.com>
2025-03-06 22:02:12 -0400

    tela inicial abrindo - porém feia demais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './navigation/AppNavigator';
 
 async function loadFonts() {
   await Font.loadAsync({
@@ -19,20 +22,10 @@ export default function App() {
   if (!fontsLoaded) {
     return null; // Ou um componente de loading
   }
-  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! - teste</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,17 +1,30 @@
-// navigation/AppNavigator.js
+// src/navigation/AppNavigator.js
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen';
-import ReportScreen from '../screens/ReportScreen';
-import MapScreen from '../screens/MapScreen';
+import TelaInicial from '../screens/TelaInicial';
+import TelaLogin from '../screens/TelaLogin';
+import TelaCadastro from '../screens/TelaCadastro';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Report" component={ReportScreen} />
-      <Stack.Screen name="Map" component={MapScreen} />
+    <Stack.Navigator initialRouteName="TelaInicial">
+      <Stack.Screen
+        name="TelaInicial"
+        component={TelaInicial}
+        options={{ title: 'Bem-vindo' }}
+      />
+      <Stack.Screen
+        name="TelaLogin"
+        component={TelaLogin}
+        options={{ title: 'Login' }}
+      />
+      <Stack.Screen
+        name="TelaCadastro"
+        component={TelaCadastro}
+        options={{ title: 'Cadastro' }}
+      />
     </Stack.Navigator>
   );
 }
