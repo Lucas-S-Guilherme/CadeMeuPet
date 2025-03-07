b378cc1daaa2b0435a4416607fee9c858b259a9f
Lucas Guilherme <lucassguilherme159@gmail.com>
2025-03-06 20:05:18 -0400

    adicionada mais dependências

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+async function loadFonts() {
+  await Font.loadAsync({
+    ...Ionicons.font,
+  });
+}
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts().then(() => setFontsLoaded(true));
+  }, []);
+
+  if (!fontsLoaded) {
+    return null; // Ou um componente de loading
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! - teste</Text>
